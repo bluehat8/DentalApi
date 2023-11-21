80fba2130296f94737e58a643cce2b1de9d12020
Agustín Amaya <80552327+bluehat8@users.noreply.github.com>
2023-11-20 22:28:21 -0600

    Controlador de solicitud cita-Primera fase

---
+++ Controllers/SolicitudCitaController.cs
@@ -0,0 +1,49 @@
+﻿using DentalApi.DTOs;
+using DentalApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentalApi.Controllers
+{
+    [ApiController]
+    [Route("api/citas")]
+    public class CitasController : ControllerBase
+    {
+        private readonly DentalContext _dbContext;
+
+        public CitasController(DentalContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+   
+
+        [HttpGet("solicitudes-cita/{usuarioId}")]
+        public async Task<IActionResult> GetSolicitudesCitaByUsuarioId(int usuarioId)
+        {
+            // Validar usuario
+            var usuario = await _dbContext.Usuarios
+                                      .Where(u => u.Id == usuarioId && u.Rol == 1)
+                                      .FirstOrDefaultAsync();
+
+            if (usuario == null)
+                return BadRequest("Usuario no encontrado o no es cliente");
+
+            var solicitudesDto = await (from c in _dbContext.Clientes
+                                        join sc in _dbContext.SolicitudCita
+                                        on c.Id equals sc.PacienteId
+                                        where c.Usuario == usuarioId
+                                        select new SolicitudCitumDto
+                                        {
+                                            Id = sc.Id,
+                                            Fecha = sc.Fecha,
+                                            Hora = sc.Hora,
+                                            TipoCita = sc.TipoCita,
+                                            MotivoCita = sc.MotivoCita,
+                                            Estado = sc.Estado,
+                                        }).ToListAsync();
+
+            return Ok(solicitudesDto);
+        }
+    }
+}
---
+++ DTOs/SolicitudCitumDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DentalApi.DTOs
+{
+    public class SolicitudCitumDto
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public TimeSpan Hora { get; set; }
+        public int TipoCita { get; set; }
+        public string? MotivoCita { get; set; }
+        public int Estado { get; set; }
+    }
+}
